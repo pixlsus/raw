d1bf26b08f9b2a1fe5a91c6440a34c9f0332d079
Kees Guequierre <kg@kees.nl>
2016-12-30 18:28:40 +0100

    Change datapath, to reflect previous commit.

--- config.example.php
+++ config.example.php
@@ -3,12 +3,11 @@
     define('dbuser','raw');
     define('dbpw','somecrazysecretpassword');
     define('dbname','raw');
-    
+
     define('dbdsn','mysql:dbname='.dbname.';host='.dbhost.';charset=UTF8');
 
-    //datapath needs to be within the website
-    define('datapath','/srv/www/raw.dtstyle.net/www/data');
+    //It's recommende to put the data dir outside the document root to prevent abuse.
+    define('datapath','/srv/www/raw.dtstyle.net/data');
     define('hashdepth',3);
-    
+
     define('baseurl','https://raw.dtstyle.net');
-    
