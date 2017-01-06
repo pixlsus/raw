abbc7259248d79934f12aea78be66501884d27ce
Kees Guequierre <kg@kees.nl>
2017-01-06 15:15:09 +0100

    workaroundandarounandaround

--- www/button-cameras.php
+++ www/button-cameras.php
@@ -2,7 +2,5 @@
     include("../config.php");
     include("functions.php");
 
-    header("Content-Type: image/svg+xml");
-
     $cameras=raw_getnumberofcameras();
-    readfile("http://img.shields.io/badge/cameras-".$cameras."-green.svg");
+    header("Location: https://img.shields.io/badge/cameras-".$cameras."-green.svg");
--- www/button-samples.php
+++ www/button-samples.php
@@ -2,7 +2,5 @@
     include("../config.php");
     include("functions.php");
 
-    header("Content-Type: image/svg+xml");
-
     $samples=raw_getnumberofsamples();
-    readfile("http://img.shields.io/badge/samples-".$samples."-green.svg");
+    header("Location: https://img.shields.io/badge/samples-".$samples."-green.svg");
