c0fb35286d868f6f8b44e8b49f79cb75792099a7
Kees Guequierre <kg@kees.nl>
2017-01-06 22:54:26 +0100

    some cache controll

--- www/button-cameras.php
+++ www/button-cameras.php
@@ -3,4 +3,7 @@
     include("functions.php");
 
     $cameras=raw_getnumberofcameras();
+
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: Sun, 25 Aug 1991 05:00:00 GMT");
     header("Location: https://img.shields.io/badge/cameras-".$cameras."-green.svg?maxAge=3600");
--- www/button-samples.php
+++ www/button-samples.php
@@ -3,4 +3,7 @@
     include("functions.php");
 
     $samples=raw_getnumberofsamples();
+
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: Sun, 25 Aug 1991 05:00:00 GMT");
     header("Location: https://img.shields.io/badge/samples-".$samples."-green.svg?maxAge=3600");
