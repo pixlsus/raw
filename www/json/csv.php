517b4c729a939f6bb33a4c8e9cd484580a7b5b1a
Kees Guequierre <kg@kees.nl>
2017-01-07 19:20:26 +0100

    csv export

---
+++ www/json/csv.php
@@ -0,0 +1,12 @@
+<?php
+    header("Content-type: text/csv");
+    include_once "../../config.php";
+    include_once "../functions.php";
+
+    $raws=raw_getalldata();
+    $i=0;
+    foreach($raws as $raw){
+        if($raw['validated']==1) {
+            echo $raw['id'].',"'.$raw['make'].'","'.$raw['model'].'","'.$raw['mode'].'","'.$raw['remark'].'","'.$raw['license'].'","'.$raw['filename'].'","'.$raw['checksum'].'","https://raw.pixls.us/getfile.php/'.$raw['id'].'/raw/'.$raw['filename']."\n";
+        }
+    }
