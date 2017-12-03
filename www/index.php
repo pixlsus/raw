3f68256e174fb3c2fccb66edd99c6b80a1eef733
Roman Lebedev <lebedev.ri@gmail.com>
2017-12-03 19:32:42 +0300

    Add two IIQ modes that we know of.

--- www/index.php
+++ www/index.php
@@ -141,6 +141,7 @@
 						<li><b>Canon (CHDK)</b>: Both the old-style CRW, and new DNG.</li>
 						<li><b>Leica</b>: Both compressed and uncompressed and all the crops (1:1, 4:3, 3:2, 16:9).</li>
 						<li><b>Pentax</b>: Both PEF and DNG.</li>
+						<li><b>Phase One A/S</b>, <b>Leaf</b> (IIQ): IIQ-L, IIQ-S.</li>
                         <li><b>Fujifilm</b>: Both the old uncompressed RAF and new compressed RAF (if supported).</li>
 					</ul>
 
