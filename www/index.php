42394908a24242bf1d3cf63c12c4b9e6c5088180
andabata <kg@kees.nl>
2018-09-06 21:36:11 +0200

    piwik.kees.nl -> piwik.pixls.us

--- www/index.php
+++ www/index.php
@@ -253,14 +253,14 @@ var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-    var u="//piwik.kees.nl/";
+    var u="//piwik.pixls.us/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '15']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
         </script>
-        <noscript><p><img src="//piwik.kees.nl/piwik.php?idsite=15&rec=1" style="border:0;" alt="" /></p></noscript>
+        <noscript><p><img src="//piwik.pixls.us/piwik.php?idsite=15&rec=1" style="border:0;" alt="" /></p></noscript>
         <!-- End Piwik Code -->
 
     </body>
