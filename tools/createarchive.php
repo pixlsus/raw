9b76325184ea8b5876c6d7c05070692ba00540fe
Kees Guequierre <kg@kees.nl>
2024-03-01 21:36:47 +0100

    Update createarchive.php; Yeah nah, no more zipfile
    Archive got to large to be meaningful. But script does do other housekeeping jobs.

--- tools/createarchive.php
+++ tools/createarchive.php
@@ -9,65 +9,50 @@
 
     $allknowncameras = array();
     foreach($cameradata as $make) {
-      if(!is_array($make))
-        continue;
+        if(!is_array($make))
+            continue;
+        foreach($make as $model) {
+            if(!is_array($model) || count($model) != 2)
+                continue;
 
-      foreach($make as $model) {
-        if(!is_array($model) || count($model) != 2)
-          continue;
+            $thecamera = array($model['make'], $model['model']);
+            if(in_array($thecamera, $allknowncameras))
+                continue;
 
-        $thecamera = array($model['make'], $model['model']);
-        if(in_array($thecamera, $allknowncameras))
-          continue;
-
-        $allknowncameras[] = $thecamera;
-      }
-    }
-
-    $zip = new ZipArchive;
-    $result = $zip->open(datapath.'/raw_pixls_us_archive.tmp.zip', ZipArchive::CREATE);
-    $allexistingsamples = array();
-    if ($result === TRUE) {
-        $zip->addFile('data.txt', 'entryname.txt');
-
-        $data=raw_getalldata();
-
-        foreach($data as $raw){
-            if($raw['validated']==1){
-                $make="unknown";
-                $model="unknown";
-                if($raw['make']!=""){
-                    $make=$cameradata[$raw['make']][$raw['model']]['make'] ?? $cameradata[$raw['make']]['make'] ?? $raw['make'];
-                }
-                if($raw['model']!=""){
-                    $model=$cameradata[$raw['make']][$raw['model']]['model'] ?? $raw['model'];
-                }
-
-                $thecamera = array($make, $model);
-                if(!in_array($thecamera, $allexistingsamples))
-                  $allexistingsamples[] = $thecamera;
-
-                $make = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $make);
-                $model = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $model);
-                echo "adding ".datapath."/".hash_id($raw['id'])."/".$raw['id']."/".$raw['filename']." as ".$make."/".$model."/".$raw['filename']."\n";
-                $zip->addFile(datapath."/".hash_id($raw['id'])."/".$raw['id']."/".$raw['filename'],$make."/".$model."/".$raw['filename']);
-            }
+            $allknowncameras[] = $thecamera;
         }
-        $zip->close();
-        rename(datapath.'/raw_pixls_us_archive.tmp.zip', datapath.'/raw_pixls_us_archive.zip');
-        echo "created new archive\n";
-    } else {
-        echo "right.. that didn't go as expected\n";
     }
 
-  $allmissingcameras = array();
-  foreach($allknowncameras as $knowncamera) {
-    if(in_array($knowncamera, $allexistingsamples))
-      continue;
+    $allexistingsamples = array();
+    $data=raw_getalldata();
 
-    if(in_array($knowncamera, $allmissingcameras))
-      continue;
+    foreach($data as $raw){
+    if($raw['validated']==1){
+        $make="unknown";
+        $model="unknown";
+        if($raw['make']!=""){
+            $make=$cameradata[$raw['make']][$raw['model']]['make'] ?? $cameradata[$raw['make']]['make'] ?? $raw['make'];
+        }
+        if($raw['model']!=""){
+            $model=$cameradata[$raw['make']][$raw['model']]['model'] ?? $raw['model'];
+        }
 
-    $allmissingcameras[] = $knowncamera;
-  }
-  file_put_contents(datapath."/missingcameradata.serialize",serialize($allmissingcameras));
+        $thecamera = array($make, $model);
+        if(!in_array($thecamera, $allexistingsamples))
+            $allexistingsamples[] = $thecamera;
+
+            $make = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $make);
+            $model = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $model);
+        }
+    }
+    $allmissingcameras = array();
+    foreach($allknowncameras as $knowncamera) {
+        if(in_array($knowncamera, $allexistingsamples))
+            continue;
+
+        if(in_array($knowncamera, $allmissingcameras))
+            continue;
+        
+        $allmissingcameras[] = $knowncamera;
+    }
+    file_put_contents(datapath."/missingcameradata.serialize",serialize($allmissingcameras));
